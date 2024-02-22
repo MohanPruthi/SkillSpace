bc9ce852c69c6d04516ac748bd61b3749610d296
MohanPruthi <mohanpruthi33@gmail.com>
2024-02-22 23:28:15 +0530

    publish1

--- src/components/core/dashboard/addCourse/PublishCourse/index.jsx
+++ src/components/core/dashboard/addCourse/PublishCourse/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const PublishCourse = () => {
+    const {register, handleSubmit, setValue, getValues} = useForm()
     return (
         <div>
             <div className='text-white'>
                 lllss
-
                 aaa
             </div>
             
