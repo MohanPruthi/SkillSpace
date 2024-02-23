e5f67d677ab99959265f536d7702264ebd1f1215
MohanPruthi <mohanpruthi33@gmail.com>
2024-02-23 23:51:34 +0530

    publish

---
+++ src/components/core/dashboard/InstructorCourses/CoursesTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoursesTable = ({courses, setCourses}) => {
+    return (
+        <div>
+            aa
+        </div>
+    )
+}
+
+export default CoursesTable
--- src/components/core/dashboard/MyCourses.jsx
+++ src/components/core/dashboard/MyCourses.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { VscAdd } from "react-icons/vsc"
+import { useSelector } from 'react-redux'
+import IconButton from '../../common/IconButton';
+import CoursesTable from './InstructorCourses/CoursesTable';
+import { useNavigate } from 'react-router-dom';
+import { fetchInstructorCourses } from '../../../services/operations/courseDetailsAPI';
+
 
 const MyCourses = () => {
+
+    const {token} = useSelector((state)=>state.auth);
+    const navigate = useNavigate();
+    const [courses, setCourses] = useState([]);
+
+    useEffect(()=> {
+        const fetchCourses = async() => {
+            const result = await fetchInstructorCourses(token);
+            if(result){
+                setCourses(result);
+            }
+        }
+        fetchCourses()
+    },[])
     return (
         <div className='text-white'>
-            aaaa
+            <div>
+                <h1 className="text-3xl font-medium text-richblack-5">My Courses</h1>
+                <IconButton
+                outline="true"
+                text="Add Course"
+                onclick={() => navigate("/dashboard/add-course")}
+                >
+                    <VscAdd />
+                </IconButton>
+            </div>
+
+            {courses && <CoursesTable courses={courses} setCourses={setCourses} />}
         </div>
     )
 }
