96684d234618f2eb39abb8ad3fbf83dd955c9326
MohanPruthi <mohanpruthi33@gmail.com>
2024-03-09 23:46:34 +0530

    g

--- src/components/core/viewCourse/VideoDetailsSidebar.jsx
+++ src/components/core/viewCourse/VideoDetailsSidebar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const VideoDetailsSidebar = ({setReviewModal}) => {
+    const {courseId, sectionId, subSectionId} = useParams();
     return (
         <div>
-            a
+
         </div>
     )
 }
