48670ae27286e6b0d44629330d0808afce6d9faf
MohanPruthi <mohanpruthi33@gmail.com>
2024-03-09 23:50:42 +0530

    video side bar error

--- src/components/core/viewCourse/VideoDetailsSidebar.jsx
+++ src/components/core/viewCourse/VideoDetailsSidebar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useAsyncError, useLocation, useNavigate, useParams, useSearchParams } from 'react-router-dom'
 
 const VideoDetailsSidebar = ({setReviewModal}) => {
-    const {courseId, sectionId, subSectionId} = useParams();
+
+    const {sectionId, subSectionId} = useParams();
+    const [activeStatus, setActiveStatus] = useState("");
+    const [videoBarActive, setVideoBarActive] = useState("")
+    const navigate = useNavigate();
+    const location = useLocation();
+    const {courseSectionData, courseEntireData, totalNoOfLectures, completedLectures, } = useSelector((state)=>state.viewCourse);
+
+
     return (
         <div>
 
